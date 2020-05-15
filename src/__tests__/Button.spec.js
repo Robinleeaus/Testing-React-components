6ff330f9ff9f24c3e53ad335968c8eec0efba720
Robin Lee <robinleeaus@gmail.com>
2020-05-15 19:32:00 +1000

    testing the wrong way

--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,15 +1,44 @@
 import React from "react";
 import { create } from "react-test-renderer";
-// import TestRenderer from "react-test-renderer";
 
-function Button(props) {
-    return <button>Nothing to do for now</button>;
+// function Button(props) {
+//     return <button>Nothing to do for now</button>;
+//   }
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+    this.handleClick = this.handleClick.bind(this);
   }
-  
+
+  handleClick() {
+    this.setState(() => {
+      return { text: "PROCEED TO CHECKOUT" };
+    });
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.text || this.props.text}
+      </button>
+    );
+  }
+}
+
+
 describe("Button component", () => {
-  test("Matches the snapshot", () => {
-    const button = create(<Button />);
-    // const button = TestRenderer.create(<Button />);
-    expect(button.toJSON()).toMatchSnapshot();
+  // test("Matches the snapshot", () => {
+  //   const button = create(<Button />);
+  //   expect(button.toJSON()).toMatchSnapshot();
+  // });
+
+  test("it shows the expected text when clicked (testing the wrong way!)", () => {
+    const component = create(<Button text="SUBSCRIBE TO BASIC" />);
+    const instance = component.getInstance();
+    expect(instance.state.text).toBe("");
+    instance.handleClick();
+    expect(instance.state.text).toBe("PROCEED TO CHECKOUT");
   });
 });
